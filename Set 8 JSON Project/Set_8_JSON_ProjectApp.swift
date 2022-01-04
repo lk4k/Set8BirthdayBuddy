1c24c6edcce65d51894673106fa5d8f0e301ac69
Lauren Kelz <s015459@students.lmsd.org>
2022-01-04 12:13:13 -0500

    Initial Commit

---
+++ Set 8 JSON Project/Set_8_JSON_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_8_JSON_ProjectApp.swift
+//  Set 8 JSON Project
+//
+//  Created by Lauren Kelz (student LM) on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Set_8_JSON_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
